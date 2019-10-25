4105a7cd046ea74bc946bbf8b9de0d5a7345bb6a
Dhairya Surana <dhairs7@vt.edu>
2019-10-25 19:30:03 -0400

    Renamed worm object to snake and made it wander horizontally

--- ECE4525_MiniProject6.js
+++ ECE4525_MiniProject6.js
@@ -6,74 +6,102 @@ var sketchProc=function(processingInstance){ with (processingInstance){
     // beginShape + endShape to enclose the entire shape
     // EXPERIMENT with coloring and number of iterations
 
-    var worm_points = [];
+    angleMode = "radians"
+
+    var snake_points = [];
     var p2 = [];
     var numPoints = 0;
 
-    var anti_gravity_worm = function(x, y) {
+    var anti_gravity_snake = function(x, y) {
 
         this.x = x;
         this.y = y;
+
+        this.direction = 0 // initially set to left
 
         this.points = [
 
             // this.x = 118
             // this.y = 62
-            new PVector(this.x + 58, this.y + 23),
-            new PVector(this.x + 89, this.y + 47),
-            new PVector(this.x + 115, this.y + 94),
-            new PVector(this.x + 117, this.y + 133),
-            new PVector(this.x + 50, this.y + 173),
-            new PVector(this.x + 55, this.y + 193),
-            new PVector(this.x + 138, this.y + 125),
-            new PVector(this.x + 133, this.y + 64),
-            new PVector(this.x + 86, this.y + 22),
-            new PVector(this.x + 65, this.y - 19),
-            new PVector(this.x + 20, this.y - 20),
-            new PVector(this.x, this.y),
-            new PVector(this.x + 2, this.y + 32),
-            new PVector(this.x + 16, this.y + 40),
-            new PVector(this.x + 25, this.y + 25),
-            new PVector(this.x + 34, this.y + 6),
-            new PVector(this.x + 57, this.y + 21),
+            new PVector(58, 23),
+            new PVector(89, 47),
+            new PVector(115, 94),
+            new PVector(117, 133),
+            new PVector(50, 173),
+            new PVector(55, 193),
+            new PVector(138, 125),
+            new PVector(133,64),
+            new PVector(86, 22),
+            new PVector(65, -19),
+            new PVector(20, -20),
+            new PVector(0, 0),
+            new PVector(2, 32),
+            new PVector(16, 40),
+            new PVector(25, 25),
+            new PVector(34, 6),
+            new PVector(57, 21),
         ];
 
     };
 
-   
-
-    anti_gravity_worm.prototype.draw = function() {
+    anti_gravity_snake.prototype.draw = function() {
 
         for(var i = 0; i < this.points.length; i++) {
             if(!pointDist(this.points[i].x, this.points[i].y)) {
-                worm_points.push(new PVector(this.points[i].x, this.points[i].y));
+                snake_points.push(new PVector(this.points[i].x, this.points[i].y));
             }
         }
 
-        
+        pushMatrix();
         translate(this.x, this.y);
         scale(0.5);
+
         fill(0,0,0);
         beginShape();
             
-            for (var i = 0; i < worm_points.length; i++) {
-                vertex(worm_points[i].x, worm_points[i].y);       
-                
+            for (var i = 0; i < snake_points.length; i++) {
+                vertex(snake_points[i].x, snake_points[i].y);       
             }    
             
-            vertex(worm_points[0].x, worm_points[0].y);
+            vertex(snake_points[0].x, snake_points[0].y);
             
         endShape();
+        popMatrix();
         
         fill(255, 0, 0);
-        ellipse(this.x + 10, this.y + 10, 10, 10);  // Eye
+        ellipse(this.x + 5, this.y + 10, 5, 5);  // Eye
+       
+        
     };
+
+    anti_gravity_snake.prototype.wander = function() {
+
+        switch(this.direction) {
+
+            case 0: //left
+                    this.x-=0.5;
+            break;
+
+            case 1: //right 
+                    this.x+=0.5;
+            break;
+
+        }
+        if (this.x === 20) {
+            this.direction = 1;
+        }
+
+        if (this.x === 310){
+            this.direction = 0;
+        }
+    };
+
 
     var pointDist = function(x, y) {
 
         var result = false;
-        for (var i = 0; i < worm_points.length; i++) {
-            if (dist(x, y, worm_points[i].x, worm_points[i].y) < 10) {
+        for (var i = 0; i < snake_points.length; i++) {
+            if (dist(x, y, snake_points[i].x, snake_points[i].y) < 10) {
                 result = true;  
             }    
         }    
@@ -98,13 +126,13 @@ var sketchProc=function(processingInstance){ with (processingInstance){
 
         p2.splice(0, p2.length);
 
-        for (var i = 0; i < worm_points.length - 1; i++) {
-            p2.push(new PVector(worm_points[i].x, worm_points[i].y));
-            p2.push(new PVector((worm_points[i].x + worm_points[i+1].x)/2, (worm_points[i].y + worm_points[i+1].y)/2));
+        for (var i = 0; i < snake_points.length - 1; i++) {
+            p2.push(new PVector(snake_points[i].x, snake_points[i].y));
+            p2.push(new PVector((snake_points[i].x + snake_points[i+1].x)/2, (snake_points[i].y + snake_points[i+1].y)/2));
         }  
         
-        p2.push(new PVector(worm_points[i].x, worm_points[i].y));
-        p2.push(new PVector((worm_points[0].x + worm_points[i].x)/2, (worm_points[0].y + worm_points[i].y)/2));
+        p2.push(new PVector(snake_points[i].x, snake_points[i].y));
+        p2.push(new PVector((snake_points[0].x + snake_points[i].x)/2, (snake_points[0].y + snake_points[i].y)/2));
     
     };  
 
@@ -115,11 +143,11 @@ var sketchProc=function(processingInstance){ with (processingInstance){
             var y = (p2[i].y + p2[i+1].y)/2;
             p2[i].set(x, y);
         } 
-        var x = (p2[i].x + worm_points[0].x)/2;
-        var y = (p2[i].y + worm_points[0].y)/2;
-        worm_points.splice(0, worm_points.length);
+        var x = (p2[i].x + snake_points[0].x)/2;
+        var y = (p2[i].y + snake_points[0].y)/2;
+        snake_points.splice(0, snake_points.length);
         for (i = 0; i < p2.length; i++) {
-            worm_points.push(new PVector(p2[i].x, p2[i].y));   
+            snake_points.push(new PVector(p2[i].x, p2[i].y));   
         }    
     };    
 
@@ -129,23 +157,26 @@ var sketchProc=function(processingInstance){ with (processingInstance){
         average();
     };    
 
-    var WORM = new anti_gravity_worm(118, 82);
+    var SNAKE = new anti_gravity_snake(118, 82);
     
     var draw = function() {
 
         background(24,252,0);
         draw_fence();
     
+        SNAKE.wander();
+        SNAKE.draw();
         
-        WORM.draw();
 
         // DO NOT change iterations limit (makes web page unresponsive)
         if (iterations < 5) {
             subdivide();
             iterations++;
         }    
-     
+
     };
+
+   
 
 }};
 
