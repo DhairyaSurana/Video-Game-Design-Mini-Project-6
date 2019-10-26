496e59ea450f33463367c86667a4a468bff7a043
Dhairya Surana <dhairs7@vt.edu>
2019-10-25 22:25:11 -0400

    Added a tail object (still needs to be connected to snake properly)

--- ECE4525_MiniProject6.js
+++ ECE4525_MiniProject6.js
@@ -12,6 +12,28 @@ var sketchProc=function(processingInstance){ with (processingInstance){
     var p2 = [];
     var numPoints = 0;
 
+    var tail = function(x, y) {
+        this.x = x; 
+        this.y = y;
+    };
+
+     //x1 = 62
+     var x1 = 62;
+     var y1 = 120;
+     var cx1 = 262;
+     var cy1 = 138;
+     var cx2 = 92;
+     var cy2 = 287;
+     var x2 = 206;
+     var y2 = 220;
+     
+     var cx1Dir = 1;
+     var cx2Dir = -1;
+     var x2Dir = -40;
+
+   
+
+    
     var anti_gravity_snake = function(x, y) {
 
         this.x = x;
@@ -44,6 +66,46 @@ var sketchProc=function(processingInstance){ with (processingInstance){
 
     };
 
+    var SNAKE = new anti_gravity_snake(118, 82);
+    var TAIL = new tail(0, 0);
+
+    tail.prototype.draw = function() {
+
+        noFill();
+        stroke(0, 0, 0);
+        bezier(x1, y1, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 2, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 4, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 6, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 8, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 10, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 12, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 14, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 16, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 18, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 20, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 22, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 24, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 26, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 28, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 30, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 32, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 34, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 36, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 38, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 40, cx1, cy1, cx2, cy2, x2, y2);
+        bezier(x1, y1 + 42, cx1, cy1, cx2, cy2, x2, y2);
+        cx1 += cx1Dir;
+        if ((cx1 > x2) || (cx1 < x1)) {cx1Dir = -cx1Dir;}
+        cx2 += cx2Dir;
+        if ((cx2 < x1) || (cx2 > x2)) {cx2Dir = -cx2Dir;}
+        x2 += x2Dir;
+        if ((abs(x1 - x2) > 200) || (x2 < x1)) {x2Dir = -x2Dir;}
+        
+        
+
+    };
+
     anti_gravity_snake.prototype.draw = function() {
 
         for(var i = 0; i < this.points.length; i++) {
@@ -53,24 +115,40 @@ var sketchProc=function(processingInstance){ with (processingInstance){
         }
 
         pushMatrix();
-        translate(this.x, this.y);
-        scale(0.5);
+            translate(this.x, this.y);
+            scale(0.5);
 
-        fill(0,0,0);
-        beginShape();
+            fill(0,0,0);
+            beginShape();
+                
+                for (var i = 0; i < snake_points.length; i++) {
+                    vertex(snake_points[i].x, snake_points[i].y);       
+                }    
+                
+                vertex(snake_points[0].x, snake_points[0].y);
+
+                
             
-            for (var i = 0; i < snake_points.length; i++) {
-                vertex(snake_points[i].x, snake_points[i].y);       
-            }    
-            
-            vertex(snake_points[0].x, snake_points[0].y);
-            
-        endShape();
+                
+            endShape();
+            // TAIL.x = this.x;
+            // TAIL.y = this.y;
+        
         popMatrix();
         
         fill(255, 0, 0);
         ellipse(this.x + 5, this.y + 10, 5, 5);  // Eye
-       
+        
+        pushMatrix();
+            
+            translate(this.x, this.y);
+            
+            scale(0.5);
+          
+            TAIL.draw();
+
+        popMatrix();
+        
         
     };
 
@@ -93,6 +171,13 @@ var sketchProc=function(processingInstance){ with (processingInstance){
 
         if (this.x === 310){
             this.direction = 0;
+        }
+
+        if(frameCount % 2 === 0) {
+            this.y-=2;
+        }
+        else {
+            this.y+=2;
         }
     };
 
@@ -157,15 +242,17 @@ var sketchProc=function(processingInstance){ with (processingInstance){
         average();
     };    
 
-    var SNAKE = new anti_gravity_snake(118, 82);
+   
     
     var draw = function() {
 
         background(24,252,0);
         draw_fence();
-    
+        
+       
         SNAKE.wander();
         SNAKE.draw();
+        
         
 
         // DO NOT change iterations limit (makes web page unresponsive)
